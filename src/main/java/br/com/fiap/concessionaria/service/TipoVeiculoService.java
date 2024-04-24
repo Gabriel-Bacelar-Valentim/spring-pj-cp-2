90db3a38446be04b74dbf4e9140d6af9fcfd84d8
gabriel-bacelar-valentim <gabrielvalentimxd@gmail.com>
2024-04-24 12:14:58 -0300

    falta-veiculoService e resources

--- src/main/java/br/com/fiap/concessionaria/service/TipoVeiculoService.java
+++ src/main/java/br/com/fiap/concessionaria/service/TipoVeiculoService.java
@@ -1,4 +1,37 @@
 package br.com.fiap.concessionaria.service;
 
-public class TipoVeiculoService {
+import br.com.fiap.concessionaria.dto.request.TipoVeiculoRequest;
+import br.com.fiap.concessionaria.dto.response.TipoVeiculoResponse;
+import br.com.fiap.concessionaria.entity.TipoVeiculo;
+import br.com.fiap.concessionaria.repository.TipoVeiculoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import java.util.Collection;
+public class TipoVeiculoService implements ServiceDTO<TipoVeiculo, TipoVeiculoRequest, TipoVeiculoResponse> {
+    @Autowired
+    private TipoVeiculoRepository repo;
+    @Override
+    public Collection<TipoVeiculo> findAll(Example<TipoVeiculo> example) {
+        return repo.findAll();
+    }
+    @Override
+    public TipoVeiculo findById(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+    @Override
+    public TipoVeiculo save(TipoVeiculo e) {
+        return repo.save(e);
+    }
+    @Override
+    public TipoVeiculo toEntity(TipoVeiculoRequest dto) {
+        return TipoVeiculo.builder()
+                .nome(dto.nome())
+                .build();
+    }
+    @Override
+    public TipoVeiculoResponse toResponse(TipoVeiculo e) {
+        return TipoVeiculoResponse.builder()
+                .nome(e.getNome())
+                .build();
+    }
 }
--- src/main/java/br/com/fiap/concessionaria/service/VeiculoService.java
+++ src/main/java/br/com/fiap/concessionaria/service/VeiculoService.java
@@ -1,4 +1,35 @@
 package br.com.fiap.concessionaria.service;
 
-public class VeiculoService {
+import br.com.fiap.concessionaria.dto.request.VeiculoRequest;
+import br.com.fiap.concessionaria.dto.response.VeiculoResponse;
+import br.com.fiap.concessionaria.entity.Veiculo;
+import org.springframework.data.domain.Example;
+
+import java.util.Collection;
+
+public class VeiculoService implements ServiceDTO<Veiculo, VeiculoRequest, VeiculoResponse>{
+    @Override
+    public Collection<Veiculo> findAll(Example<Veiculo> example) {
+        return null;
+    }
+
+    @Override
+    public Veiculo findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Veiculo save(Veiculo e) {
+        return null;
+    }
+
+    @Override
+    public Veiculo toEntity(VeiculoRequest dto) {
+        return null;
+    }
+
+    @Override
+    public VeiculoResponse toResponse(Veiculo e) {
+        return null;
+    }
 }
